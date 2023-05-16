f371b21270f8cf298f5af77d6efecaa7ad553280
Paul Winkler <slinkp@gmail.com>
2023-05-16 13:05:42 -0400

    Fix doctests for py 3

--- spydey/patternize.py
+++ spydey/patternize.py
@@ -9,33 +9,33 @@ Examples:
 >>> patternize('')
 ''
 
->>> print patternize('9')
+>>> print(patternize('9'))
 \d+
 >>> re.match(patternize('9'), '27') is None
 False
 
->>> print patternize('aBc-')
+>>> print(patternize('aBc-'))
 [-a-zA-Z_]+
 >>> re.match(patternize('aBc-'), 'a_b_c-dEFG') is None
 False
 
->>> print patternize('hello world')
+>>> print(patternize('hello world'))
 [-a-zA-Z_]+\s+[-a-zA-Z_]+
 >>> re.match(patternize('hello world'), 'gooDbye MoOn') is None
 False
 
->>> print patternize('hello world!!?!...')
+>>> print(patternize('hello world!!?!...'))
 [-a-zA-Z_]+\s+[-a-zA-Z_]+\W+
 >>> re.match(patternize('hello world!!?!...'), 'oh nooo!!!') is None
 False
 
->>> print patternize('\\njohn Q. public,\\nat-large')
+>>> print(patternize('\\njohn Q. public,\\nat-large'))
 \s+[-a-zA-Z_]+\s+[-a-zA-Z_]+\W+\s+[-a-zA-Z_]+\W+\s+[-a-zA-Z_]+
 >>> re.match(patternize('\\njohn Q. public,\\nat-large'),
 ...          '\\njohn Q. public,\\nat-large') is None
 False
 
->>> print patternize('_bag_of_potatos__')
+>>> print(patternize('_bag_of_potatos__'))
 [-a-zA-Z_]+
 >>> re.match(patternize('_bag_of_potatos__'), '_asdpOPI-UB_') is None
 False
@@ -79,9 +79,7 @@ def patternize(astring):
         re_obj = pat_to_re[pat]
         substituted = [replace_pattern_with_re_obj(re_obj, s) for s in output]
         output = flatten(substituted)
-        #print '%r %r %r' % (pat, re_map[pat], re_map[pat].sub('X', s))
 
-    #import pprint; pprint.pprint(output)
     pattern_list = [re_to_pat[obj] for obj in output if obj]
     return ''.join(pattern_list)
 
